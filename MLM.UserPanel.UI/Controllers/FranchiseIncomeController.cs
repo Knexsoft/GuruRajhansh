66cac5d892746bde51ff88db3ca6e1c4008b40b8
Kanha <mr.kanhaiyaa@outlook.com>
2018-08-29 20:31:16 +0530

    add franchiseincome and singlelegincome controller

---
+++ MLM.UserPanel.UI/Controllers/FranchiseIncomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MLM.UserPanel.UI.Controllers
+{
+    public class FranchiseIncomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddFranchise()
+        {
+            return View();
+        }
+    }
+}
